f184f22819328d85636dbd422724a7eb6f9ecf99
Nicolas Reibel <nre@2crsi.com>
2019-08-09 11:38:45 +0200

    First commit

---
+++ files/pm-wrap.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+function usage()
+{
+    echo "usage: $0 [search|install|update|remove] <package>"
+    echo "       $0 [update-all]"
+}
+
+case "$1" in
+    search)
+        case `basename $0` in
+            apt-pm)
+                apt-cache search  $2
+                ;;
+            pip-pm)
+                pip search $2
+                ;;
+            snap-pm)
+                snap search $2
+                ;;
+            *)
+                echo "Unsupported package manager"
+                usage
+                ;;
+        esac
+        ;;
+    install)
+        case `basename $0` in
+            apt-pm)
+                apt-get install $2
+                ;;
+            pip-pm)
+                pip install $2
+                ;;
+            snap-pm)
+                snap install $2
+                ;;
+            *)
+                echo "Unsupported package manager"
+                usage
+                ;;
+        esac
+        ;;
+    update)
+        case `basename $0` in
+            apt-pm)
+                apt-get update && apt-get upgrade $2
+                ;;
+            pip-pm)
+                pip install --upgrade $2
+                ;;
+            snap-pm)
+                snap refresh $2
+                ;;
+            *)
+                echo "Unsupported package manager"
+                usage
+                ;;
+        esac
+        ;;
+    remove)
+        case `basename $0` in
+            apt-pm)
+                apt-get remove $2
+                ;;
+            pip-pm)
+                pip uninstall $2
+                ;;
+            snap-pm)
+                snap remove $2
+                ;;
+            *)
+                echo "Unsupported package manager"
+                usage
+                ;;
+        esac
+        ;;
+    update-all)
+        case `basename $0` in
+            apt-pm)
+                apt-get update && apt-get upgrade
+                ;;
+            pip-pm)
+                for pkg in `pip list --outdated | tail -n +3 | awk '{print $1}'`
+                do
+                    pip install --upgrade $pkg
+                done
+                ;;
+            snap-pm)
+                snap refresh
+                ;;
+            *)
+                echo "Unsupported package manager"
+                usage
+                ;;
+        esac
+        ;;
+    *)
+        echo "Unsupported operation"
+        usage
+        ;;
+esac
+    
---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash 
+
+SCRIPT_NAME='pm-wrap.sh'
+INSTALL_DIR='/usr/bin/'
+
+# Check root
+if [ $EUID -ne 0 ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+function install()
+{
+    cp "./files/$SCRIPT_NAME" "$INSTALL_DIR"
+    ln -s "$INSTALL_DIR/$SCRIPT_NAME" "$INSTALL_DIR/apt-pm" 2>/dev/null
+    ln -s "$INSTALL_DIR/$SCRIPT_NAME" "$INSTALL_DIR/pip-pm" 2>/dev/null
+    ln -s "$INSTALL_DIR/$SCRIPT_NAME" "$INSTALL_DIR/snap-pm" 2>/dev/null
+}
+
+function remove()
+{
+    rm "$INSTALL_DIR/$SCRIPT_NAME" "$INSTALL_DIR/apt-pm" "$INSTALL_DIR/pip-pm" "$INSTALL_DIR/snap-pm"
+}
+
+case $1 in
+    remove)
+        remove
+        echo "Uninstalled"
+        ;;
+    install)
+        ;&
+    *)
+        install
+        echo "Installed"
+        ;;
+esac
